48ce161236f59a168d28ec297b16a7ef9efcef82
Gerardo (Jerry) Quintero <7450202+jerryq27@users.noreply.github.com>
2024-06-13 17:01:01 -0500

    Replaced uniqid with a uuid function.
    The chance of a conflict with uniqid(); are a bit higher than I'm comfortable with.

--- src/ACF_Field_Unique_ID.php
+++ src/ACF_Field_Unique_ID.php
@@ -61,6 +61,18 @@ class ACF_Field_Unique_ID extends acf_field {
 			return $value;
 		}
 
-		return uniqid();
+		return uuidv4();
+	}
+
+	/**
+         * Function to create a unique UUID
+	 */
+	function uuidv4() {
+		$data = random_bytes(16);
+		
+		$data[6] = chr(ord($data[6]) & 0x0f | 0x40); // set version to 0100
+		$data[8] = chr(ord($data[8]) & 0x3f | 0x80); // set bits 6-7 to 10
+		
+		return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
 	}
 }
